efdc008afbd52c5f1cae6b6e8b2344407779073a
Brooks Naylor <brooksnaylor@gmail.com>
2018-04-06 16:17:59 -0500

    Finish Commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('../database/index.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!');
+});
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+const App = () => (
+	<div>Hello World!</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
